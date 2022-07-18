ec261a361cca11bed76416dabb224f0cc1a606d7
maryam <maryam.askari@irori.se>
2022-07-18 13:57:08 +0200

    Changed some parts of the code

--- src/main/java/com/example/bank/api/CustomerAPI.java
+++ src/main/java/com/example/bank/api/CustomerAPI.java
@@ -2,12 +2,14 @@ package com.example.bank.api;
 
 import com.example.bank.model.entity.Customer;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,54 +23,53 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-@Api(tags = {"Customer"})
+@Tag(name = "customer")
 @RequestMapping("/customer")
 public interface CustomerAPI {
 
   @Operation(summary = "Get all customers")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "OK!", content = {@Content(mediaType = "list"
+      @ApiResponse(responseCode = "200", description = "OK!", content = {@Content(mediaType = "application/list"
           , schema = @Schema(implementation = Customer.class))}),
       @ApiResponse(responseCode = "400", description = "Bad request!", content = @Content),
       @ApiResponse(responseCode = "404", description = "Not found!", content = @Content)})
-  @GetMapping("/customers")
-  public ResponseEntity<List<Customer>> getAllCustomer();
+  @GetMapping("/get-customers")
+  ResponseEntity<List<Customer>> getAllCustomer();
 
   @Operation(summary = "Add a new customer")
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "OK!", content = {@Content(mediaType = "application/json"
           , schema = @Schema(implementation = Customer.class))}),
       @ApiResponse(responseCode = "400", description = "Bad request!", content = @Content),
-      @ApiResponse(responseCode = "404", description = "Not found!", content = @Content)})
+      @ApiResponse(responseCode = "405", description = "Invalid input", content = @Content)})
   @PostMapping("/add-customer")
-  public ResponseEntity<Customer> addCustomer(@ApiParam(value = "New customer to add", required = true)
+  ResponseEntity<Customer> addCustomer(@ApiParam(value = "New customer to add", required = true)
                                               @Valid @RequestBody Customer customer);
 
-  @Operation(summary = "Get customer by id")
+  @Operation(summary = "Find customer by ID", description = "Return a single customer")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "OK!", content = {@Content(mediaType = "application/json"
+      @ApiResponse(responseCode = "200", description = "Successful operation", content = {@Content(mediaType = "application/json"
           , schema = @Schema(implementation = Customer.class))}),
-      @ApiResponse(responseCode = "400", description = "Bad request!", content = @Content),
-      @ApiResponse(responseCode = "404", description = "Not found!", content = @Content)})
+      @ApiResponse(responseCode = "400", description = "Invalid ID supplied", content = @Content),
+      @ApiResponse(responseCode = "404", description = "Customer not found", content = @Content)})
   @GetMapping("/find-customer/{customerId}")
-  public ResponseEntity<Customer> getCustomerById(@ApiParam(value = "Id of customer", required = true)
+  ResponseEntity<Customer> getCustomerById(@ApiParam(value = "Id of customer", required = true)
                                                   @PathVariable("customerId") Long customerId);
-  @Operation(summary = "Update customer by id")
+  @Operation(summary = "Update an existing customer")
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "OK!", content = {@Content(mediaType = "application/json"
           , schema = @Schema(implementation = Customer.class))}),
-      @ApiResponse(responseCode = "400", description = "Bad request!", content = @Content),
-      @ApiResponse(responseCode = "404", description = "Not found!", content = @Content)})
+      @ApiResponse(responseCode = "400", description = "Invalid ID supplied", content = @Content),
+      @ApiResponse(responseCode = "404", description = "Customer not found", content = @Content),
+      @ApiResponse(responseCode = "500", description = "Validation exception", content = @Content)})
   @PutMapping("/update-customer")
-  public ResponseEntity updateCustomer(@ApiParam(value = "Customer for updating") Customer customer);
+  ResponseEntity updateCustomer(@ApiParam(value = "Customer for updating", required = true) Customer customer);
 
-  @Operation(summary = "Delete customer by id")
+  @Operation(summary = "Deletes a customer")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "OK!", content = {@Content(mediaType = "application/json"
-          , schema = @Schema(implementation = Customer.class))}),
-      @ApiResponse(responseCode = "400", description = "Bad request!", content = @Content),
-      @ApiResponse(responseCode = "404", description = "Not found!", content = @Content)})
+      @ApiResponse(responseCode = "400", description = "Invalid ID supplied", content = @Content),
+      @ApiResponse(responseCode = "404", description = "Customer not found", content = @Content)})
   @DeleteMapping("/delete-customer/{customerId}")
-  public ResponseEntity deleteCustomer(@ApiParam(value = "customer id for deleting", required = true)
+  ResponseEntity deleteCustomer(@ApiParam(value = "customer id for deleting", required = true)
                                        @PathVariable(value = "customerId") Long customerId);
 }
