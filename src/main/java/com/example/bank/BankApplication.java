d58b554452f652afe9a53e18aa192c96172ccd57
maryam <maryam.askari@irori.se>
2022-07-18 14:20:35 +0200

    have added application.yml file and the configuration of the yaml file. Have added some properties for swagger.

--- src/main/java/com/example/bank/BankApplication.java
+++ src/main/java/com/example/bank/BankApplication.java
@@ -1,13 +1,26 @@
 package com.example.bank;
 
+import com.example.bank.config.YAMLConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankApplication {
+public class BankApplication implements CommandLineRunner {
+
+	@Autowired
+	private YAMLConfig config;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+    System.out.println("Using environment: " + config.getEnvironment());
+    System.out.println("name: " + config.getName());
+    System.out.println("enabled: " + config.isEnabled());
+    System.out.println("Services: " + config.getServers());
+	}
 }
