c4472b20c520093b7e26a6173508775a1a908ceb
maryam <maryam.askari@irori.se>
2022-07-18 13:56:22 +0200

    Have updated AccountAPI and cleaning the code.

---
+++ src/main/java/com/example/bank/api/AccountAPI.java
@@ -0,0 +1,69 @@
+package com.example.bank.api;
+
+
+import com.example.bank.model.dto.AccountDto;
+import com.example.bank.model.entity.Account;
+import com.example.bank.model.entity.Customer;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Tag(name = "Account")
+@RequestMapping("/account")
+public interface AccountAPI {
+
+  @Operation(summary = "Update an existing account")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "OK!", content = {@Content(mediaType = "application/json"
+          , schema = @Schema(implementation = AccountDto.class))}),
+      @ApiResponse(responseCode = "400", description = "Invalid ID supplied", content = @Content),
+      @ApiResponse(responseCode = "404", description = "Account not found", content = @Content),
+      @ApiResponse(responseCode = "405", description = "Validation exception", content = @Content)})
+  @PutMapping("/update-account")
+  ResponseEntity updateAccount(@Valid @ApiParam(value = "Customer for updating", required = true)
+                                            AccountDto accountDto);
+
+  @Operation(summary = "Add a new account")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "OK!", content = {@Content(mediaType = "application/json"
+          , schema = @Schema(implementation = Account.class))}),
+      @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content),
+      @ApiResponse(responseCode = "404", description = "Not found!", content = @Content)})
+  @PostMapping("add-account")
+  ResponseEntity<AccountDto> addAccount(@ApiParam(required = true)
+                                               @RequestBody AccountDto accountDto);
+
+  @Operation(summary = "Find account by ID", description = "Returns a single account")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "OK!", content = {@Content(mediaType = "application/xml"
+          , schema = @Schema(implementation = Account.class))}),
+      @ApiResponse(responseCode = "400", description = "Invalid ID supplied"),
+      @ApiResponse(responseCode = "404", description = "Account not found")})
+  @GetMapping("/get-account/{accountId}")
+  ResponseEntity<AccountDto> getAccountById(@ApiParam(value = "ID of account to return", required = true)
+                                                   @PathVariable("accountId") Long accountId);
+
+  @Operation(summary = "Deletes an account")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "400", description = "Invalid ID supplied"),
+      @ApiResponse(responseCode = "404", description = "Account not found")})
+  @DeleteMapping("/delete-account/{accountId}")
+  ResponseEntity deleteAccount(@ApiParam(value = "Account id to delete")
+                                      @PathVariable("accountId") Long accountId);
+}
--- src/main/java/com/example/bank/controller/AccountController.java
+++ src/main/java/com/example/bank/controller/AccountController.java
@@ -1,9 +1,11 @@
 package com.example.bank.controller;
 
+import com.example.bank.api.AccountAPI;
 import com.example.bank.exceptions.AccountNotFoundException;
 import com.example.bank.model.dto.AccountDto;
 import com.example.bank.model.entity.Account;
 import com.example.bank.service.AccountService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.modelmapper.ModelMapper;
@@ -11,11 +13,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/account")
-public class AccountController {
 
-    private static final Logger LOGGER = LogManager.getLogger(AccountController.class);
+@Slf4j
+@RestController
+public class AccountController implements AccountAPI {
+
 
     private ModelMapper modelMapper;
 
@@ -24,44 +26,44 @@ public class AccountController {
         this.accountService = accountService;
     }
 
-    @PutMapping
+    @Override
     public ResponseEntity updateAccount(AccountDto accountDto) {
-        LOGGER.info("Starting updateAccount method {" + accountDto.getId() + "}");
+        log.info("Starting updateAccount method {" + accountDto.getId() + "}");
         try {
             Account accountRequest = modelMapper.map(accountDto, Account.class);
             accountService.updateAccount(accountRequest.getId(), accountRequest);
             AccountDto accountResponse = modelMapper.map(accountRequest, AccountDto.class);
             return new ResponseEntity(HttpStatus.OK);
         } catch (AccountNotFoundException e) {
-            LOGGER.error(e.getMessage());
+            log.error(e.getMessage());
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
     }
 
-    @PostMapping
-    public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto) {
-        LOGGER.info("Starting addAccount method {" + accountDto.getAccountNo() + "}");
+    @Override
+    public ResponseEntity<AccountDto> addAccount(AccountDto accountDto) {
+        log.info("Starting addAccount method {" + accountDto.getAccountNo() + "}");
         Account accountRequest = modelMapper.map(accountDto, Account.class);
         Account savedAccount = accountService.registerAccount(accountRequest);
         AccountDto accountResponse = modelMapper.map(savedAccount, AccountDto.class);
         return new ResponseEntity<AccountDto>(accountResponse, HttpStatus.CREATED);
     }
 
-    @GetMapping("/{accountId}")
-    public ResponseEntity<AccountDto> getAccountById(@PathVariable("accountId") Long accountId) {
-        LOGGER.info("Starting getCustomerById method {" + accountId + "}");
+    @Override
+    public ResponseEntity<AccountDto> getAccountById(Long accountId) {
+        log.info("Starting getCustomerById method {" + accountId + "}");
         Account account = accountService.getAccountById(accountId);
         if (account != null) {
             AccountDto accountResponse = modelMapper.map(account, AccountDto.class);
-            return new ResponseEntity<AccountDto>(accountResponse, HttpStatus.OK);
+            return new ResponseEntity<>(accountResponse, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
 
-    @DeleteMapping("/{accountId}")
-    public ResponseEntity deleteAccount(@PathVariable("accountId") Long accountId) {
-        LOGGER.info("Starting deleteCustomer method {" + accountId + "}");
+    @Override
+    public ResponseEntity deleteAccount(Long accountId) {
+        log.info("Starting deleteCustomer method {" + accountId + "}");
         accountService.deleteAccountById(accountId);
         return new ResponseEntity(HttpStatus.OK);
     }
