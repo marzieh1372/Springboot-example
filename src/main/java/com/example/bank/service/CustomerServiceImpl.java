34cc7df2834dadcf56775ef41f0df81caa7536ab
maryam <maryam.askari@irori.se>
2022-08-03 17:25:27 +0200

    Bean exception handling.

--- src/main/java/com/example/bank/service/CustomerServiceImpl.java
+++ src/main/java/com/example/bank/service/CustomerServiceImpl.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
-  @Autowired private CustomerMapper mapper;
+  private CustomerMapper mapper;
 
   @Autowired private CustomerRepository customerRepository;
 
