46ec8595ffe7d99e0e8a2787e2d101122ae956e2
maryam <maryam.askari@irori.se>
2022-08-25 09:48:48 +0200

    check the merging commits

--- src/main/java/com/example/bank/mapper/AccountMapper.java
+++ src/main/java/com/example/bank/mapper/AccountMapper.java
@@ -2,10 +2,18 @@ package com.example.bank.mapper;
 
 import com.example.bank.model.dto.AccountRequest;
 import com.example.bank.model.entity.Account;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public interface AccountMapper {
-  Account accountRequestMapToAccount(AccountRequest accountRequest);
-  AccountRequest accountMapToAccountRequest(Account account);
+public abstract class AccountMapper {
+
+    @Mapping(source = "customerId", target = "customer.id")
+    public abstract Account accountRequestMapToAccount(AccountRequest accountRequest);
+
+    @InheritInverseConfiguration
+    public abstract AccountRequest accountMapToAccountRequest(Account account);
+
+
 }
--- src/main/java/com/example/bank/repo/CustomerRepository.java
+++ src/main/java/com/example/bank/repo/CustomerRepository.java
@@ -1,16 +1,7 @@
 package com.example.bank.repo;
 
-import com.example.bank.model.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
-   // @Query(value = "select * from customer where firstname=?",nativeQuery = true)
-    List<Customer> findByFirstName(String firstName);
-
-    Customer findByUserName(String userName);
+public interface CustomerRepository extends PersonRepository {
 }
